821370f857142904d23a271025ba1c24a2270c47
Joshua Hykes <jhykes@gmail.com>
2011-12-07 16:48:15 -0500

    Added Bounded_Univariate_Spline class

--- rebin.py
+++ rebin.py
@@ -6,6 +6,46 @@ Rebin histogram values.
 import numpy as np
 from numpy.random import uniform
 
+from scipy.interpolate import UnivariateSpline
+
+class Bounded_Univariate_Spline(UnivariateSpline):
+    """
+    Interpolatory spline that returns a constant for x outside the
+    specified domain.
+    """
+    def __init__(self, x, y, fill_value=0.0, **kwargs):
+        self.bnds = [x[0], x[-1]]
+        self.fill_value = fill_value
+        UnivariateSpline.__init__(self, x, y, **kwargs)
+
+    def is_outside_domain(self, x):
+        x = np.asarray(x)
+        return np.logical_or(x<self.bnds[0], x>self.bnds[1])
+
+    def __call__(self, x):
+        outside = self.is_outside_domain(x)
+
+        return np.where(outside, self.fill_value, 
+                                 UnivariateSpline.__call__(self, x))
+        
+    def integral(self, a, b):
+        # capturing contributions outside domain of interpolation
+        below_dx = np.max([0., self.bnds[0]-a])
+        above_dx = np.max([0., b-self.bnds[1]])
+
+        outside_contribution = (below_dx + above_dx) * self.fill_value
+
+        # adjusting interval to interpolatory domain
+        a_f = np.max([a, self.bnds[0]])
+        b_f = np.min([b, self.bnds[1]])
+
+        if a_f >= b_f:
+            return outside_contribution
+        else:
+            return (outside_contribution +
+                      UnivariateSpline.integral(self, a_f, b_f) )
+
+
 
 def edge_step(x, y, **kwargs):
     """
@@ -21,10 +61,65 @@ def edge_step(x, y, **kwargs):
     return plt.plot(x, np.hstack([y,y[-1]]), drawstyle='steps-post', **kwargs)
 
 
+def rebin(x1, y1, x2, interp_kind='piecewise_constant'):
+    """
+    Rebin histogram values y1 from old bin edges x1 to new edges x2.
+
+    Input
+    -----
+     * x1 : m+1 array of old bin edges.
+     * y1 : m array of old histogram values. This is the total number in 
+              each bin.
+     * x2 : n+1 array of new bin edges.
+     * interp_kind : how is the underlying unknown continuous distribution
+                      assumed to look: {'cubic', 'piecewise_constant'}
+
+    Returns
+    -------
+     * y2 : n array of rebinned histogram values.
+
+    Bins in x2 that are entirely outside the range of x1 are assigned 0.
+    """
+
+    if interp_kind == 'piecewise_constant':
+        return rebin_piecewise_constant(x1, y1, x2)
+    else:
+        return rebin_spline(x1, y1, x2, interp_kind=interp_kind)
+     
+
+def rebin_spline(x1, y1, x2, interp_kind='piecewise_constant'):
+    """
+    Rebin histogram values y1 from old bin edges x1 to new edges x2.
+
+    Input
+    -----
+     * x1 : m+1 array of old bin edges.
+     * y1 : m array of old histogram values. This is the total number in 
+              each bin.
+     * x2 : n+1 array of new bin edges.
+     * interp_kind : how is the underlying unknown continuous distribution
+                      assumed to look: {'cubic'}
+
+    Returns
+    -------
+     * y2 : n array of rebinned histogram values.
+
+    The rebinning algorithm assumes that the counts in each old bin are
+    uniformly distributed in that bin.
+
+    Bins in x2 that are entirely outside the range of x1 are assigned 0.
+    """
+
+    # midpoints of x1
+    x1_mid = x1[:-1] + np.ediff1d(x1)
+
+    # constructing data for spline
+    xx = np.hstack([x1[0], x1_mid, x1[-1]])
+    yy = np.hstack([y1[0], y1, y1[-1]])
 
 
 
-def rebin(x1, y1, x2):
+def rebin_piecewise_constant(x1, y1, x2):
     """
     Rebin histogram values y1 from old bin edges x1 to new edges x2.
 
